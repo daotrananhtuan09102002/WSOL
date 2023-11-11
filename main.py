c3cc289836ef0916e9717b3a90f57e818f9c0f91
daotrananhtuan09102002 <79463278+daotrananhtuan09102002@users.noreply.github.com>
2023-11-11 16:11:21 +0700

    add tqdm

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ def main():
                  large_feature_map = True, drop_threshold = 0.8, drop_prob = 0.25, lr = 0.002, lr_classifier_ratio = 10.0,
                  momentum = 0.9, weight_decay = 0.0001, lr_decay_points = [41, 61], lr_decay_rate = 0.2,
                  sim_fg_thres = 0.4, sim_bg_thres = 0.2, loss_ratio_drop = 2.0,
-                 loss_ratio_sim = 0.5, loss_ratio_norm = 0.05, wsol_method = 'bridging-gap', loader = voc_dataloader, log_dir = '/conte')
+                 loss_ratio_sim = 0.5, loss_ratio_norm = 0.05, wsol_method = 'bridging-gap', 
+                 loader = voc_dataloader, log_dir = '/content/')
 
 
 if __name__ == '__main__':
